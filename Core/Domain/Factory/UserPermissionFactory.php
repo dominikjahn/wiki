48f6c9e657080e844722715c1447d33f54159e85
dominikjahn <dominik1991jahn@gmail.com>
2015-07-15 21:53:28 +0200

    Prepared User permission management

---
+++ Core/Domain/Factory/UserPermissionFactory.php
@@ -0,0 +1,64 @@
+<?php
+	/**
+	 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+	 * @version 0.1
+	 * @since 0.1
+	 */
+	class UserPermissionFactory extends DomainFactory
+	{
+		  //
+		 // METHODS
+		//
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		public function FromDataRow(Domain $object, DatabaseRow $row) {
+			$userManager = UserManager::GetInstance();
+			$logManager = LogManager::GetInstance();
+			
+			$object->ID = $row->userpermission_id->Integer;
+			$object->Status = $row->status->Integer;
+			$object->User = $userManager->GetByID($row->user_id->Integer);
+			$object->Permission = $row->permission->String;
+			
+			$object->LogCreated = $logManager->GetByObjectAndType($object, Log::TYPE_CREATE);
+			$object->LogModified = $logManager->GetByObjectAndType($object, Log::TYPE_MODIFY);
+			
+			if(is_null($object->LogModified)) {
+				$object->LogModified = $object->LogCreated;
+			}
+			
+			if($object->Status === 0) {
+			  $object->LogDeleted = $logManager->GetByObjectAndType($object, Log::TYPE_DELETE);
+			}
+			
+			$object->IsLoadedFromDatabase = true;
+		}
+		
+		  //
+		 // FUNCTIONS
+		//
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		public static function GetInstance() {
+			if(!self::$instance) {
+				self::$instance = new VersionFactory();
+			}
+			
+			return self::$instance;
+		}
+		
+		  //
+		 // VARIABLES
+		//
+		
+		private static $instance;
+	}
+?>
---
+++ Core/Domain/UserPermission.php
@@ -0,0 +1,110 @@
+<?php
+	/**
+	 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+	 * @version 0.1
+	 * @since 0.1
+	 */
+	class UserPermission extends Domain
+	{
+		  //
+		 // CONSTRUCTOR
+		//
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		public function __construct() {
+			
+		}
+		
+		  //
+		 // METHODS
+		//
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		public function MatchPassword($password) {
+			if($this->password == $password) {
+				return true;
+			}
+			
+			return false;
+		}
+		
+		public function jsonSerialize() {
+			return [
+				"user" => $this->user->loginname,
+				"permission" => $this->permission
+			];
+		}
+		
+		  //
+		 // ATTRIBUTES
+		//
+		
+		/**
+		 * @field user_id
+		 */
+		protected $user;
+		
+		/**
+		 * @field permission
+		 */
+		protected $permission;
+		
+		  //
+		 // GETTERS / SETTERS
+		//
+		
+		# User
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		protected function GetUser() {
+			return $this->user;
+		}
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		protected function SetUser(User $value) {
+			$this->user = $value;
+		}
+		
+		# Permission
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		protected function GetPermission() {
+			return $this->permission;
+		}
+		
+		/**
+		 * @author Dominik Jahn <dominik1991jahn@gmail.com>
+		 * @version 0.1
+		 * @since 0.1
+		 */
+		protected function SetPermission($value) {
+			$this->permission = $value;
+		}
+		
+		  //
+		 // CONSTANTS
+		//
+		
+		const DB_TABLE = "userpermission";
+	}
+?>
