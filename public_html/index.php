659121f16b5e0fbd18c815569bb282f38bdf4521
dominikjahn <dominik1991jahn@gmail.com>
2015-08-20 21:17:22 +0200

    Android/iOS shortcuts open in fullscreen mode

--- public_html/index.php
+++ public_html/index.php
@@ -15,6 +15,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 		
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="mobile-web-app-capable" content="yes" />
+		
 		<title>Wiki</title>
 		
 		<base href="<?php echo Wiki\Configuration::WWW_ROOT; ?>" />
